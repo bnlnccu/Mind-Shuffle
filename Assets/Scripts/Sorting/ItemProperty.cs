635a8a2fbaf907796ad37f6b5b2e9b352f242a06
Roger Liu <6roger@gmail.com>
2026-05-10 23:00:11 +0800

    feat: add ItemProperty -- pure data tag for stimulus Prefabs

---
+++ Assets/Scripts/Sorting/ItemProperty.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class ItemProperty : MonoBehaviour
+{
+    [Header("===== Stimulus Properties =====")]
+
+    [Tooltip("Category of this object (Fruit or Animal)")]
+    [SerializeField] private ItemCategory category;
+
+    [Tooltip("Color of this object (Red or Blue)")]
+    [SerializeField] private ItemColor color;
+
+    public ItemCategory Category => category;
+    public ItemColor Color => color;
+}
